2ce3da7b007d20f51169a065f467e2d2e810de7c
Stef432 <stefaniaurriza@gmail.com>
2024-03-06 19:22:11 +0100

    created cleaner and availability tables

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
 });
 
+router.post("/", function(req, res, next) {
+  res.send({title:"Express"})
+})
+
+CREATE TABLE `cleaners`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `phone_number` INT NOT NULL,
+  `tax_id` INT NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `cleaners_availability_id` INT NOT NULL
+);
+
+CREATE TABLE `cleaner_availability`(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `days` DATE NOT NULL,
+  `time_range` TIME NOT NULL,
+  `frequency` VARCHAR(255) NOT NULL,
+  `cleaner_id` INT NOT NULL,
+  `type_of_service` VARCHAR(255) NOT NULL
+);
+
+
+
 module.exports = router;
